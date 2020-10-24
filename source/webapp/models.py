82c39fe47bb1b1e14db9638420f22642bff66072
Kurmanbek <kkmambetov@gmail.com>
2020-10-24 11:51:42 +0600

    Создан модель Photo

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Photo(models.Model):
+    picture = models.ImageField(upload_to='uploads', verbose_name='Картинка')
+    text = models.TextField(max_length=3000, null=True, blank=True, verbose_name='Подпись')
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    author = models.ForeignKey(get_user_model(), on_delete=models.SET_DEFAULT, default=1,
+                               related_name='articles', verbose_name='Автор')
+
+    def __str__(self):
+        return f'{self.text} {self.author}'
+
+    class Meta:
+        verbose_name = 'Картинка'
+        verbose_name_plural = 'Картинки'
+
